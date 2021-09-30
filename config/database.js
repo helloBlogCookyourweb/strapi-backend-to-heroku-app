2a73bd2f47bb0e0ed6bb5fc2ad1ed393167d937f
helloBlogCookyourweb <hello.blogcookyourweb@gmail.com>
2021-09-30 16:23:42 +0200

    fix to deploy

--- config/database.js
+++ config/database.js
@@ -13,33 +13,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
-      },
-    }
-  }
-
-}
